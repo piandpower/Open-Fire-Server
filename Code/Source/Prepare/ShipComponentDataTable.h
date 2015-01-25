78deb6761508d7a25d1d838a2bd864ff95b83edb
rtNavy <jaewan.huey.Park@gmail.com>
2015-01-25 23:52:19 +0900

    Change ship_component_data / Create Weapon AI BT, BB

--- Code/Source/Prepare/ShipComponentDataTable.h
+++ Code/Source/Prepare/ShipComponentDataTable.h
@@ -12,23 +12,11 @@ struct FShipComponentDataTable : public FTableRowBase
 	FString ship_name;
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = ShipComponentData)
+	int32 index;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = ShipComponentData)
 	FString type;
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = ShipComponentData)
-	float position_x;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = ShipComponentData)
-	float position_y;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = ShipComponentData)
-	float position_z;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = ShipComponentData)
-	float rotation_x;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = ShipComponentData)
-	float rotation_y;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = ShipComponentData)
-	float rotation_z;
+	FString size;
 };
