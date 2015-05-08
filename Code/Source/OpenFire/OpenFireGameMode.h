8157ee2e664c768fcb5311740fc96c3236751118
HueyPark <jaewan.huey.Park@gmail.com>
2015-05-08 23:25:13 +0900

    add: new project

---
+++ Code/Source/OpenFire/OpenFireGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "OpenFireGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class OPENFIRE_API AOpenFireGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
