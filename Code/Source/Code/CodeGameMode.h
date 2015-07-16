b5550df63205bebdc1b012c2d0a6366cae3b8dd9
HueyPark <jaewan.huey.Park@gmail.com>
2015-07-16 17:26:12 +0900

    Create new project

---
+++ Code/Source/Code/CodeGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "CodeGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CODE_API ACodeGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
