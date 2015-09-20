22ec174ee7b148650b0d5d72bfe36d750024b7f2
HueyPark <jaewan.huey.park@gmail.com>
2015-09-20 13:25:13 +0900

    Change folder structures, Add ShipManager class

---
+++ Code/Source/OpenFire/Managers/ShipManager.h
@@ -0,0 +1,13 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+/**
+ * 
+ */
+class OPENFIRE_API ShipManager
+{
+public:
+	ShipManager();
+	~ShipManager();
+};
