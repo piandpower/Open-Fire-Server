85a91e94151508ff1b0f4f7b0037008aa501c6c2
HueyPark <jaewan.huey.park@gmail.com>
2015-10-11 18:08:24 +0900

    Add staticmesh, collision to Bullet

--- Code/Source/OpenFire/Bullets/Bullet.cpp
+++ Code/Source/OpenFire/Bullets/Bullet.cpp
@@ -10,6 +10,20 @@ ABullet::ABullet()
     // Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
     PrimaryActorTick.bCanEverTick = true;
 
+    UArrowComponent* pArrow = CreateDefaultSubobject<UArrowComponent>(TEXT("UArrowComponent"));
+    pArrow->AttachTo(RootComponent);
+
+    UStaticMeshComponent* pUStaticMeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("UStaticMeshComponent"));
+    pUStaticMeshComponent->AttachTo(pArrow);
+    static ConstructorHelpers::FObjectFinder<UStaticMesh> VisualAsset(TEXT("/Engine/BasicShapes/Sphere"));
+    if (VisualAsset.Succeeded())
+    {
+        pUStaticMeshComponent->SetStaticMesh(VisualAsset.Object);
+    }
+
+    USphereComponent* pUSphereComponent = CreateDefaultSubobject<USphereComponent>(TEXT("Proximity Sphere"));
+    pUSphereComponent->AttachTo(pArrow);
+    pUSphereComponent->SetSphereRadius(50.0f);
 }
 
 // Called when the game starts or when spawned
