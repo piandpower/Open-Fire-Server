dfffa7169fda6b4242b38347d83d1664cff020e7
hueypark <hueypark@nextfloor.com>
2016-01-25 20:40:05 +0900

    Add: simple templates

--- Code/Server/openfire/gameoflife/gameoflife.py
+++ Code/Server/openfire/gameoflife/gameoflife.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 from world import World
 
 bp = Blueprint('gameoflife', __name__)
@@ -7,7 +7,4 @@ bp = Blueprint('gameoflife', __name__)
 @bp.route('/')
 @bp.route('/index')
 def index():
-    world = World()
-    for tile in world.get_tiles():
-        print(tile)
-    return 'Helo World!'
+    return render_template('index.html')
