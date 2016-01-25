f4525d5cbeb790e5029021a57c69c63cba7437fe
hueypark <hueypark@nextfloor.com>
2016-01-25 20:58:44 +0900

    Update: folder structure

--- Code/Server/openfire/gameoflife/gameoflife.py
+++ Code/Server/openfire/gameoflife/gameoflife.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template
 
-bp = Blueprint('gameoflife', __name__)
+bp = Blueprint('gameoflife', __name__, template_folder='templates', static_folder='static')
 
 
 @bp.route('/')
