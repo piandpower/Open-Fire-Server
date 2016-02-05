d6a647b1e9030926e33e7de1fa2c7b06dcb447f5
hueypark <hueypark@nextfloor.com>
2016-02-05 18:13:54 +0900

    Add: Node, Edge model

---
+++ Code/Server/openfire/gameoflife/models.py
@@ -0,0 +1,20 @@
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, Float, ForeignKey
+
+Base = declarative_base()
+
+
+class Node(Base):
+    __tablename__ = 'node'
+
+    node_id = Column(Integer, primary_key=True)
+    name = Column(String)
+    position_x = Column(Float)
+    position_y = Column(Float)
+
+
+class Edge(Base):
+    __tablename__ = 'edge'
+
+    start_node_id = Column(Integer, ForeignKey('node.node_id'), primary_key=True)
+    end_node_id = Column(Integer, ForeignKey('node.node_id'), primary_key=True)
