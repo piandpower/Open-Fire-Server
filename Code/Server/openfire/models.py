fb824b41b8b614f777a973da2be0c92fd7116d1c
HueyPark <jaewan.huey.park@gmail.com>
2016-02-05 23:08:25 +0900

    Add: test data

--- Code/Server/openfire/models.py
+++ Code/Server/openfire/models.py
@@ -13,6 +13,17 @@ def create_test_database():
     from models import Base
     Base.metadata.create_all(bind=engine)
 
+    __create_test_data()
+
+
+def __create_test_data():
+    import random
+    Session()
+    for i in range(5):
+        node = Node(name='node'+str(i), position_x=random.randrange(1, 11), position_y=random.randrange(1,11))
+        Session.add(node)
+    Session.commit()
+
 
 class Node(Base):
     __tablename__ = 'node'
