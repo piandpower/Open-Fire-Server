14a0aa378b842269a7241f84a10389de8f4d5764
HueyPark <jaewan.huey.park@gmail.com>
2016-02-05 22:50:44 +0900

    Update: set test mode

--- Code/Server/openfire/__init__.py
+++ Code/Server/openfire/__init__.py
@@ -1,9 +1,13 @@
 from flask import Flask
-
 from openfire.gameoflife import gameoflife_bp
 
 
-def create_app():
+def create_app(test=False):
     app = Flask(__name__)
     app.register_blueprint(gameoflife_bp, url_prefix='/gameoflife')
+
+    if test:
+        from models import create_test_database
+        create_test_database()
+
     return app
--- Code/Server/runserver.py
+++ Code/Server/runserver.py
@@ -1,4 +1,4 @@
 from openfire import create_app
 
-app = create_app()
+app = create_app(test=True)
 app.run(debug=True)
