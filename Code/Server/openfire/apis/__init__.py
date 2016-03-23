61d5f6e062534b943ba076a0edfaa7f3f7cd951c
hueypark <jaewan.huey.park@gmail.com>
2016-03-23 23:36:23 +0900

    Add: api GET /nodes

--- Code/Server/openfire/apis/__init__.py
+++ Code/Server/openfire/apis/__init__.py
@@ -1,5 +1,26 @@
 from flask import Blueprint
 from .node_view import NodeView
+from ..database import session
 
 apis = Blueprint('apis', __name__)
 apis.add_url_rule('/nodes', view_func=NodeView.as_view('nodes'))
+
+
+@apis.after_request
+def after_request(response):
+    try:
+        session.commit()
+    except Exception as e:
+        print(e)
+    return response
+
+
+@apis.errorhandler(Exception)
+def errorhandler(e):
+    print(e)
+    session.rollback()
+
+
+@apis.teardown_request
+def teardown_request(exception=None):
+    session.remove()
--- Code/Server/openfire/apis/node_view.py
+++ Code/Server/openfire/apis/node_view.py
@@ -1,11 +1,22 @@
 from flask import jsonify
 from flask.views import MethodView
+from ..database import session
+from ..models import Node
 
 
 class NodeView(MethodView):
     def get(self):
-        response = jsonify(
-            {
-                'node_view': 'data'
+
+        nodes = []
+        for node in session.query(Node).all():
+            nodes.append({
+                'node_id': node.node_id,
+                'name': node.name,
+                'position_x': node.position_x,
+                'position_y': node.position_y
             })
+
+        response = jsonify({
+            'nodes': nodes
+        })
         return response
