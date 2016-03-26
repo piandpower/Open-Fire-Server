0fbf029d0fec327b79209e416b2f6a9edd61f25b
hueypark <jaewan.huey.park@gmail.com>
2016-03-26 15:02:37 +0900

    Update: Generate more test data

--- Code/Server/openfire/database.py
+++ Code/Server/openfire/database.py
@@ -10,10 +10,10 @@ session = scoped_session(session_factory)
 Base.metadata.create_all(bind=engine)
 
 
-def add_test_data():
-    nodecount = 5
+def __add_test_data():
+    nodecount = 100
     for i in range(nodecount):
-        node = StrongPoint(i, 'node' + str(i), random.randrange(1, 11), random.randrange(1, 11))
+        node = StrongPoint(i, 'node' + str(i), random.randrange(-5000, 5000), random.randrange(-5000, 5000))
         session.add(node)
 
     session.add_all(
@@ -28,4 +28,4 @@ def add_test_data():
 
     session.commit()
 
-add_test_data()
+__add_test_data()
