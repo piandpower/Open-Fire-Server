8176724e1944408a690428603fd03247168dbd48
hueypark <jaewan.huey.park@gmail.com>
2016-06-13 23:10:24 +0900

    Fix: WorldGenerator __is_overlap

--- Server/Code/world/world_generator.py
+++ Server/Code/world/world_generator.py
@@ -1,7 +1,10 @@
 from typing import Iterable
+from random import uniform
 from .static_actor import Node, Edge
 
-NODE_SPACING = 10.0
+MIN_NODE_RANGE = 100.0
+SQ_MIN_NODE_RANGE = MIN_NODE_RANGE * MIN_NODE_RANGE
+MAX_EDGE_RANGE = 300.0
 ID_UNKNOWN = 'ID_INKNOWN'
 
 
@@ -13,14 +16,30 @@ class WorldGenerator:
         self.nodes = self.__generate_nodes()
         self.edges = self.__generate_edges(self.nodes)
 
-    @staticmethod
-    def __generate_nodes():
+    @classmethod
+    def __generate_nodes(cls):
         nodes = []
-        for x in range(10):
-            for y in range(10):
-                nodes.append(Node(ID_UNKNOWN, x * NODE_SPACING, y * NODE_SPACING))
+        for _ in range(10000):
+            node = cls.__generate_random_node()
+            if not cls.__is_overlap(node.x, node.y, nodes):
+                nodes.append(node)
 
         return nodes
+
+    @staticmethod
+    def __generate_random_node():
+        return Node(ID_UNKNOWN, uniform(0.0, 500.0), uniform(0.0, 500.0))
+
+    @staticmethod
+    def __is_overlap(pos_x, pos_y, nodes: Iterable[Node]):
+        for node in nodes:
+            dist_x = pos_x - node.x
+            dist_y = pos_y - node.y
+            sq_dist = (dist_x * dist_x) + (dist_y * dist_y)
+            if sq_dist <= SQ_MIN_NODE_RANGE:
+                return True
+
+        return False
 
     @classmethod
     def __generate_edges(cls, nodes: Iterable[Node]):
@@ -34,10 +53,9 @@ class WorldGenerator:
 
     @staticmethod
     def __has_edge(start_node: Node, end_node: Node):
-        node_spacing = NODE_SPACING * 1.1
         dist_x = start_node.x - end_node.x
         dist_y = start_node.y - end_node.y
-        if node_spacing * node_spacing > ((dist_x * dist_x) + (dist_y * dist_y)):
+        if MAX_EDGE_RANGE * MAX_EDGE_RANGE > ((dist_x * dist_x) + (dist_y * dist_y)):
             return False
 
         return True
