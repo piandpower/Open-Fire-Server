0598c2129f16434d58124c7404b7ac6bae9e4e09
HueyPark <jaewan.huey.park@gmail.com>
2016-07-17 13:47:07 +0900

    Add: type -> generate_cpp_header util

---
+++ Code/framework/type/util/generate_cpp_header.py
@@ -0,0 +1,19 @@
+from enum import Enum
+from framework.type import Request
+
+
+def generate_cpp_header(python_enum: Enum):
+    name = python_enum.__name__
+    with open(name + '.h', 'w') as f:
+        f.write('#pragma once\n')
+        f.write('\n')
+        f.write('enum class ' + name + '\n')
+        f.write('{')
+        enum_strings = []
+        for element in python_enum:
+            enum_strings.append('\n	' + element.name + ' = ' + str(element.value))
+        f.write(','.join(enum_strings))
+        f.write('\n}\n')
+
+if __name__ == "__main__":
+    generate_cpp_header(Request)
